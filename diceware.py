fae4a2d34bf8e311ce646797ce7894ebbd0adab0
Martin Wegner <mw@mroot.net>
2011-11-10 20:26:56 +0100

    Pretty print password, more readable this way.

--- diceware.py
+++ diceware.py
@@ -8,7 +8,9 @@ try:
 	with open( sys.argv[ 1 ], 'r' ) as f:
 		wordsLines = f.readlines()
 		words = dict( [ ( line.split()[ 0 ], line.split()[ 1 ] ) for line in wordsLines ] )
-		password = ''.join( [ words[ ''.join( [ str( random.randint( 1, 6 ) ) for i in range( 5 ) ] ) ] for i in range( int( sys.argv[ 2 ] ) ) ] )
-		print password
+		passwordWords = [ words[ ''.join( [ str( random.randint( 1, 6 ) ) for i in range( 5 ) ] ) ] for i in range( int( sys.argv[ 2 ] ) ) ]
+		print 'Chosen words:', ','.join( passwordWords )
+		password = ''.join( passwordWords )
+		print 'Yielding password:', password
 except IndexError:
 	print sys.argv[ 0 ], '<wordList>', '<numWords>'
